aeec624e2c94a1c6d2a4883f02529464a33d8f30
Gopal-sahu2001 <120116262+Gopal-sahu2001@users.noreply.github.com>
2025-03-18 00:04:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
+import gdown
 import streamlit as st
+
+url= "https://drive.google.com/file/d/1TcEQ092-zqJ9YiPMdtiICZVDdACvtGlF/view?usp=sharing"
+output = "model/model_diseases.h5"
+gdown.download(url, output, quiet=False)
+
 
 st.title("🎈 My new app")
 st.write(
