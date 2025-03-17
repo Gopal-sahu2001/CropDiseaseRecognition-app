f0a29a2c755e3ff28aa31f913b45f47cfcdeefc4
Gopal-sahu2001 <120116262+Gopal-sahu2001@users.noreply.github.com>
2025-03-18 00:52:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
 
-download_model()
-
 # Load the trained model
-model_path = "model_diseases.h5"  # Update this with your local path
+model_path = download_model()  # Update this with your local path
 model = tf.keras.models.load_model(model_path)
 
 # Define class labels
